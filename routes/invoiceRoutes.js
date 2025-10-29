03400a93761af3481a50e5a1768a877dc9c77d2a
harialakunta7-web <harialakunta7@gmail.com>
2025-10-29 13:41:22 +0530

    Add project files excluding .env

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -7,7 +7,7 @@ const { createInvoice,getInvoiceById,getInvoicesByStore } = require("../controll
 // When mounted at /api in server.js, use plain /invoices here
 router.post("/invoices", createInvoice);
 router.get("/invoices/:invoiceId", getInvoiceById);
-router.get("/stores/:storeId/invoices", getInvoicesByStore);
+router.get("/invoices/stores/:storeid", getInvoicesByStore);
 // router.get("/invoices/pdf/:invoiceId", getInvoicePDF);
 // router.patch("/invoices/:invoiceId/status", updateInvoiceStatus);
 module.exports = router;
